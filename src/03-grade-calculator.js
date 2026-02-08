86f83b9de00af78938bda9c7bcf7fa95ff7c8acf
Swagatika1982 <40235193+Swagatika1982@users.noreply.github.com>
2026-02-07 22:21:15 -0500

    Update 03-grade-calculator.js

--- src/03-grade-calculator.js
+++ src/03-grade-calculator.js
@@ -26,4 +26,40 @@
  */
 export function calculateGrade(score, hasExtraCredit) {
   // Your code here
+
+    let rewards = 5;
+  let grade = "INVALID"
+
+
+  if (typeof hasExtraCredit === "boolean" && hasExtraCredit) {
+  // hasExtraCredit is boolean and true
+    if(score <=(100-rewards))
+      score += rewards; 
+    }
+
+  
+  if( score < 0 || score > 100)  return (grade);
+
+  
+  if (score >= 90 && score <= 100) {
+    grade= "A";
+      } 
+  else if (score >= 80 && score <=89) {
+    grade= "B";
+      } 
+  else if (score >= 70 && score <= 79) {
+      grade= "C";
+      } 
+  else if (score >= 60 && score <= 69) {
+      grade= "D";
+      } 
+   else if (score >= 0 && score <= 59) {
+      grade= "E";
+      }
+  else {
+      grade= "INVALID";
+      }
+
+ return (grade);
+  
 }
